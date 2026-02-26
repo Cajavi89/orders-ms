9ba62c969666a2aa9cfb75b373072b6a88432f8d
Carlos Jaramillo <ajaramillo@magnetoglobal.com>
2026-02-26 18:11:50 -0500

    inicializacion y configuracion de microservicio

---
+++ src/main.ts
@@ -0,0 +1,29 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { Logger, ValidationPipe } from '@nestjs/common';
+import { envs } from './config/envs';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+
+async function bootstrap() {
+  const logger = new Logger('Orders-ms');
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.TCP,
+      options: {
+        port: envs.PORT,
+      },
+    },
+  );
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+  await app.listen();
+
+  logger.log(`Orders-MS is running on port ${envs.PORT}`);
+}
+bootstrap();
